54da770318e9146577bafc1611f066024a17f71b
LanLi2017 <lilan.scut@gmail.com>
2018-06-01 11:15:37 -0500

    First pages commit

--- OR2YW/use_case/clean.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# set directory variables
-source ../settings.sh
-
-rm -rf $FACTS_DIR
-rm -rf $VIEWS_DIR
-rm -rf $RESULTS_DIR
--- OR2YW/use_case/script/yw_gen_reconfacts.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-alias yw='java -jar ~/yesworkflow-0.2.2.0-SNAPSHOT-jar-with-dependencies.jar'
-
-yw recon json_python.py -c recon.comment='#' -c recon.factsfile=reconfacts.P
-
