ecacd724f180d05eda6b3360bf2e85888aae2425
LanLi2017 <lilan.scut@gmail.com>
2018-07-11 21:17:01 -0500

    updatejson

--- OpenRefine3Operations/Menu_case/Parallel&SequentialGoodModel/parsePy.py
+++ OpenRefine3Operations/Menu_case/Parallel&SequentialGoodModel/parsePy.py
@@ -1,11 +1,11 @@
 import json
 
-with open('Menu_caseJsonfile.json','rb')as f:
+with open('newJson.json','r')as f:
     data=json.load(f)
 
 newlist=sorted(data,key=lambda k:k['columnName'])
 with open('newJson.json','w')as f:
-    f.writelines(json.dumps(newlist))
+    f.writelines(json.dumps(newlist,indent=2))
 
 
 
