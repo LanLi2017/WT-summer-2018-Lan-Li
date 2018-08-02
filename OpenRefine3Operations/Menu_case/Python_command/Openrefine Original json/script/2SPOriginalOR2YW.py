0b1e6610b5b1eec7dd2cd19af3e4ea4f8d56e360
LanLi2017 <lilan.scut@gmail.com>
2018-08-02 11:06:02 -0500

    update yw

--- OpenRefine3Operations/Menu_case/Python_command/Openrefine Original json/script/2SPOriginalOR2YW.py
+++ OpenRefine3Operations/Menu_case/Python_command/Openrefine Original json/script/2SPOriginalOR2YW.py
@@ -29,13 +29,13 @@ for innerlists in list_of_lists:
 # substraction of the list of lists of dicts
 newlist_of_lists=[item for item in list_of_lists if item not in splitlists]
 
-# print all of the @in
+# print all of the #@in
 inputdatalist=[]
 for dicts in data:
-    # print('@begin '+dicts['op']+'@desc '+dicts['description']+'\n')
+    # print('#@begin '+dicts['op']+'#@desc '+dicts['description']+'\n')
     if dicts['op']=='core/column-rename':
-        # print('@in '+dicts['oldColumnName']+'\n')
-        # print('@in '+dicts['newColumnName']+'\n')
+        # print('#@in '+dicts['oldColumnName']+'\n')
+        # print('#@in '+dicts['newColumnName']+'\n')
         oldColumnName='oldColumnName:'+dicts['oldColumnName']
         newColumnName='newColumnName:'+dicts['newColumnName']
         inputdatalist.append(oldColumnName)
@@ -66,7 +66,7 @@ i=0
 for dicts in data[:rename_c]:
     if dicts['op']=='core/column-rename':
         table_counter+=1
-# f.write('@end OperationsOn%s\n'%list_of_sublists[a][0])
+# f.write('#@end OperationsOn%s\n'%list_of_sublists[a][0])
 for splitlist in splitlists:
     for splitdicts in splitlist:
         splitcol=splitdicts['columnName'].split()
@@ -78,23 +78,23 @@ outtable=table_counter+1
 
 # parse into YW model
 f=open('../yw/2Original_SPParseYW.txt','w')
-f.write('@begin SPOriginalOR2@desc Workflow of Linear original openrefine history\n')
+f.write('#@begin SPOriginalOR2#@desc Workflow of Linear original openrefine history\n')
 for sublist in list(deinputdatalist):
-    f.write('@param '+sublist+'\n')
-f.write('@in table0\n')
-f.write('@out table%d\n'%outtable)
+    f.write('#@param '+sublist+'\n')
+f.write('#@in table0\n')
+f.write('#@out table%d\n'%outtable)
 table_c=0
 i=0
 # rename operations
 for dicts in data[:rename_c]:
     if dicts['op']=='core/column-rename':
-        f.write('@begin core/column-rename%d'%i+'@desc '+dicts['description']+'\n')
-        f.write('@param oldColumnName:'+dicts['oldColumnName']+'\n')
-        f.write('@param newColumnName:'+dicts['newColumnName']+'\n')
-        f.write('@in table%d\n'%table_c)
+        f.write('#@begin core/column-rename%d'%i+'#@desc '+dicts['description']+'\n')
+        f.write('#@param oldColumnName:'+dicts['oldColumnName']+'\n')
+        f.write('#@param newColumnName:'+dicts['newColumnName']+'\n')
+        f.write('#@in table%d\n'%table_c)
         table_c+=1
-        f.write('@out table%d\n'%table_c)
-        f.write('@end core/column-rename%d\n'%i)
+        f.write('#@out table%d\n'%table_c)
+        f.write('#@end core/column-rename%d\n'%i)
         i+=1
 
 #operations on column (column level)
@@ -104,11 +104,11 @@ def ruleforreturn(ind,tc,colname,table_counter):
     # [{'columnName':Sponsor, 'function':value.toNumber()},{....}]
     # [{'columnName':event, 'function':value.toDate()},{.....}]
     if ind==0:
-       f.write('@in table%d\n'%table_counter)
-       f.write('@out %s%d\n'%(colname,tc))
+       f.write('#@in table%d\n'%table_counter)
+       f.write('#@out %s%d\n'%(colname,tc))
     else:
-        f.write('@in %s%d\n'%(colname,tc-1))
-        f.write('@out %s%d\n'%(colname,tc))
+        f.write('#@in %s%d\n'%(colname,tc-1))
+        f.write('#@out %s%d\n'%(colname,tc))
 
 
 
@@ -125,24 +125,24 @@ for lists in newlist_of_lists:
     for dicts in lists:
         col_name='col:%s'%dicts['columnName']
         if dicts['op']=='core/mass-edit':
-            f.write('@begin core/mass-edit%d'%massedit_c+'@desc '+dicts['description']+'\n')
-            f.write('@param col-name:'+dicts['columnName']+'\n')
+            f.write('#@begin core/mass-edit%d'%massedit_c+'#@desc '+dicts['description']+'\n')
+            f.write('#@param col-name:'+dicts['columnName']+'\n')
             ruleforreturn(count,tc,col_name,table_c)
             tc+=1
             count+=1
-            f.write('@end core/mass-edit%d\n'%massedit_c)
+            f.write('#@end core/mass-edit%d\n'%massedit_c)
             massedit_c+=1
         elif dicts['op']=='core/text-transform':
-            f.write('@begin core/text-transform%d'%texttrans_c+'@desc '+dicts['description']+'\n')
-            f.write('@param col-name:'+dicts['columnName']+'\n')
-            f.write('@param expression:'+dicts['expression']+'\n')
+            f.write('#@begin core/text-transform%d'%texttrans_c+'#@desc '+dicts['description']+'\n')
+            f.write('#@param col-name:'+dicts['columnName']+'\n')
+            f.write('#@param expression:'+dicts['expression']+'\n')
             ruleforreturn(count,tc,col_name,table_c)
             tc+=1
             count+=1
-            f.write('@end core/text-transform%d\n'%texttrans_c)
+            f.write('#@end core/text-transform%d\n'%texttrans_c)
             texttrans_c+=1
 
-    # f.write('@end OperationsOn%s\n'%list_of_sublists[a][0])
+    # f.write('#@end OperationsOn%s\n'%list_of_sublists[a][0])
 colsplit_c=0
 dtable_c=table_c
 for splitlist in splitlists:
@@ -151,53 +151,53 @@ for splitlist in splitlists:
     for splitdicts in splitlist:
         splitcol=splitdicts['columnName'].split()
         if len(splitcol)==1:
-            f.write('@begin core/column-split%d'%colsplit_c+'@desc %s\n'%(splitdicts['description'])+'\n')
-            f.write('@param separator:"%s"\n'%(splitdicts['separator']))
-            f.write('@param removeOriginalColumn:%s\n'%splitdicts['removeOriginalColumn'])
-            f.write('@in %s\n'%splitdicts['columnName'])
-            f.write('@in table%d\n'%table_c)
+            f.write('#@begin core/column-split%d'%colsplit_c+'#@desc %s\n'%(splitdicts['description'])+'\n')
+            f.write('#@param separator:"%s"\n'%(splitdicts['separator']))
+            f.write('#@param removeOriginalColumn:%s\n'%splitdicts['removeOriginalColumn'])
+            f.write('#@in %s\n'%splitdicts['columnName'])
+            f.write('#@in table%d\n'%table_c)
             dtable_c+=1
-            f.write('@out table%d\n'%dtable_c)
-            f.write('@end core/column-split%d\n'%colsplit_c)
+            f.write('#@out table%d\n'%dtable_c)
+            f.write('#@end core/column-split%d\n'%colsplit_c)
             colsplit_c+=1
         elif len(splitcol)>1:
             colformsplitname='col:%s'%(splitdicts['columnName'].split()[0])
             if splitdicts['op']=='core/mass-edit':
-                f.write('@begin core/mass-edit%d'%massedit_c+'@desc '+splitdicts['description']+'\n')
-                f.write('@param col-name:"%s"\n'%splitdicts['columnName'])
+                f.write('#@begin core/mass-edit%d'%massedit_c+'#@desc '+splitdicts['description']+'\n')
+                f.write('#@param col-name:"%s"\n'%splitdicts['columnName'])
                 ruleforreturn(splitindex,splitc,colformsplitname,dtable_c)
                 splitc+=1
                 splitindex+=1
-                f.write('@end core/mass-edit%d\n'%massedit_c)
+                f.write('#@end core/mass-edit%d\n'%massedit_c)
                 massedit_c+=1
             elif splitdicts['op']=='core/text-transform':
-                f.write('@begin core/text-transform%d'%texttrans_c+'@desc '+splitdicts['description']+'\n')
-                f.write('@param col-name:'+splitdicts['columnName']+'\n')
-                f.write('@param expression:'+splitdicts['expression']+'\n')
+                f.write('#@begin core/text-transform%d'%texttrans_c+'#@desc '+splitdicts['description']+'\n')
+                f.write('#@param col-name:'+splitdicts['columnName']+'\n')
+                f.write('#@param expression:'+splitdicts['expression']+'\n')
                 ruleforreturn(splitindex,splitc,colformsplitname,dtable_c)
                 splitc+=1
                 splitindex+=1
-                f.write('@end core/text-transform%d\n'%texttrans_c)
+                f.write('#@end core/text-transform%d\n'%texttrans_c)
                 texttrans_c+=1
 
 
-f.write('@begin MergeOperationsColumns @desc Merge the Parallel Column operations\n')
+f.write('#@begin MergeOperationsColumns #@desc Merge the Parallel Column operations\n')
 for a in range(len(newlist_of_lists)):
     newcol_name='col:%s'%(newlist_of_lists[a][0]['columnName'])
     colcounter=len(newlist_of_lists[a])-1
-    f.write('@in %s%d\n'%(newcol_name,colcounter))
+    f.write('#@in %s%d\n'%(newcol_name,colcounter))
 
 
 for b in range(len(splitlists)):
     if len(splitlists[b])==1:
-        f.write('@in table%d'%dtable_c)
+        f.write('#@in table%d'%dtable_c)
     elif len(splitlists[b])>1:
         splitcol_name='col:%s'%(splitlists[b][0]['columnName'])
         splitcounter=len(splitlists[b])-2
-        f.write('@in %s%d\n'%(splitcol_name,splitcounter))
+        f.write('#@in %s%d\n'%(splitcol_name,splitcounter))
 outtable=dtable_c+1
-f.write('@out table%d\n'%outtable)
-f.write('@end MergeOperationsColumns\n')
+f.write('#@out table%d\n'%outtable)
+f.write('#@end MergeOperationsColumns\n')
 
-f.write('@end SPOriginalOR2\n')
+f.write('#@end SPOriginalOR2\n')
 f.close()
--- OpenRefine3Operations/Menu_case/Python_command/Openrefine-client reproducible json/script/Linear_XOR2YW.py
+++ OpenRefine3Operations/Menu_case/Python_command/Openrefine-client reproducible json/script/Linear_XOR2YW.py
@@ -8,10 +8,10 @@ inputdatalist=[]
 with open('ExtendedWF.json','r')as f:
     data=json.load(f)
     for dicts in data:
-        # print('@begin '+dicts['op']+'@desc '+dicts['description']+'\n')
+        # print('#@begin '+dicts['op']+'#@desc '+dicts['description']+'\n')
         if dicts['op']=='core/column-rename':
-            # print('@in '+dicts['oldColumnName']+'\n')
-            # print('@in '+dicts['newColumnName']+'\n')
+            # print('#@in '+dicts['oldColumnName']+'\n')
+            # print('#@in '+dicts['newColumnName']+'\n')
             oldColumnName='oldColumnName:'+dicts['oldColumnName']
             newColumnName='newColumnName:'+dicts['newColumnName']
             inputdatalist.append(oldColumnName)
@@ -43,17 +43,17 @@ with open('ExtendedWF.json','r')as f:
 deinputdatalist=set(inputdatalist)
 
 # for sublist in list(deinputdatalist):
-#     print('@in '+sublist)
+#     print('#@in '+sublist)
 
 # for the subset of the procedure
 
 # parse and print it out
 f=open('../yw/XLinearParseYW.txt','w')
-f.write('@begin LinearXOR@desc Workflow of Extended openrefine history\n')
+f.write('#@begin LinearXOR#@desc Workflow of Extended openrefine history\n')
 for sublist in list(deinputdatalist):
-    f.write('@param '+sublist+'\n')
-f.write('@in table0\n')
-f.write('@out table%d\n'%(len(data)-1))
+    f.write('#@param '+sublist+'\n')
+f.write('#@in table0\n')
+f.write('#@out table%d\n'%(len(data)-1))
 rename_c=0
 massedit_c=0
 texttrans_c=0
@@ -61,47 +61,47 @@ colsplit_c=0
 table_c=0
 for dicts in data:
     if dicts['op']=='core/column-rename':
-        f.write('@begin core/column-rename%d'%rename_c+'@desc '+dicts['description']+'\n')
-        f.write('@param oldColumnName:'+dicts['oldColumnName']+'\n')
-        f.write('@param newColumnName:'+dicts['newColumnName']+'\n')
-        f.write('@in table%d\n'%table_c)
+        f.write('#@begin core/column-rename%d'%rename_c+'#@desc '+dicts['description']+'\n')
+        f.write('#@param oldColumnName:'+dicts['oldColumnName']+'\n')
+        f.write('#@param newColumnName:'+dicts['newColumnName']+'\n')
+        f.write('#@in table%d\n'%table_c)
         table_c+=1
-        f.write('@out table%d\n'%table_c)
-        f.write('@end core/column-rename%d\n'%rename_c)
+        f.write('#@out table%d\n'%table_c)
+        f.write('#@end core/column-rename%d\n'%rename_c)
 
         rename_c+=1
 
     elif dicts['op']=='core/mass-edit':
-        f.write('@begin core/mass-edit%d'%massedit_c+'@desc '+dicts['description']+'\n')
-        f.write('@param col-name:'+dicts['columnName']+'\n')
-        f.write('@param cluster-type:%s\n'%dicts['Cluster-type'])
-        f.write('@param cluster-params:%s\n'%dicts['Cluster-params'])
-        f.write('@param cluster-function:%s\n'%dicts['Cluster-function'])
-        f.write('@in table%d\n'%table_c)
+        f.write('#@begin core/mass-edit%d'%massedit_c+'#@desc '+dicts['description']+'\n')
+        f.write('#@param col-name:'+dicts['columnName']+'\n')
+        f.write('#@param cluster-type:%s\n'%dicts['Cluster-type'])
+        f.write('#@param cluster-params:%s\n'%dicts['Cluster-params'])
+        f.write('#@param cluster-function:%s\n'%dicts['Cluster-function'])
+        f.write('#@in table%d\n'%table_c)
         table_c+=1
-        f.write('@out table%d\n'%table_c)
-        f.write('@end core/mass-edit%d\n'%massedit_c)
+        f.write('#@out table%d\n'%table_c)
+        f.write('#@end core/mass-edit%d\n'%massedit_c)
         massedit_c+=1
     elif dicts['op']=='core/text-transform':
-        f.write('@begin core/text-transform%d'%texttrans_c+'@desc '+dicts['description']+'\n')
-        f.write('@param col-name:'+dicts['columnName']+'\n')
-        f.write('@param expression:'+dicts['expression']+'\n')
-        f.write('@in table%d\n'%table_c)
+        f.write('#@begin core/text-transform%d'%texttrans_c+'#@desc '+dicts['description']+'\n')
+        f.write('#@param col-name:'+dicts['columnName']+'\n')
+        f.write('#@param expression:'+dicts['expression']+'\n')
+        f.write('#@in table%d\n'%table_c)
         table_c+=1
-        f.write('@out table%d\n'%table_c)
-        f.write('@end core/text-transform%d\n'%texttrans_c)
+        f.write('#@out table%d\n'%table_c)
+        f.write('#@end core/text-transform%d\n'%texttrans_c)
         texttrans_c+=1
     elif dicts['op']=='core/column-split':
-        f.write('@begin core/column-split%d'%colsplit_c+'@desc '+dicts['description']+'\n')
-        f.write('@param col-name:'+dicts['columnName']+'\n')
-        f.write('@param separator:'+'"%s"'%(dicts['separator'])+'\n')
-        f.write('@param removeOriginalColumn:%s\n'%dicts['removeOriginalColumn'])
-        f.write('@in table%d\n'%table_c)
+        f.write('#@begin core/column-split%d'%colsplit_c+'#@desc '+dicts['description']+'\n')
+        f.write('#@param col-name:'+dicts['columnName']+'\n')
+        f.write('#@param separator:'+'"%s"'%(dicts['separator'])+'\n')
+        f.write('#@param removeOriginalColumn:%s\n'%dicts['removeOriginalColumn'])
+        f.write('#@in table%d\n'%table_c)
         table_c+=1
-        f.write('@out table%d\n'%table_c)
-        f.write('@end core/column-split%d\n'%colsplit_c)
+        f.write('#@out table%d\n'%table_c)
+        f.write('#@end core/column-split%d\n'%colsplit_c)
         colsplit_c+=1
 
 
-f.write('@end LinearXOR\n')
+f.write('#@end LinearXOR\n')
 f.close()
